9befcdb90b336bae61e66c257a6654c9d1756047
Alicja Begović <alicja.begovic@gmail.com>
2024-07-27 14:56:47 +0200

    adding new sound and images

--- src/source.js
+++ src/source.js
@@ -1,7 +1,7 @@
 import cena_pl_i from "./assets/img/cena_pl.svg";
 import cena_cz_i from "./assets/img/cena_cz.svg";
 import czar_pl_i from "./assets/img/czar.svg";
-import car_cz_i from "./assets/img/car.svg";
+import car_cz_i from "./assets/img/car-cz-s.svg";
 import droga_pl_i from "./assets/img/droga_pl.svg";
 import droga_cz_i from "./assets/img/droga_cz.svg";
 import zly_pl_i from "./assets/img/zly_pl.svg";
@@ -18,6 +18,12 @@ import walka_pl_i from "./assets/img/walka.svg";
 import valka_cz_i from "./assets/img/valka.svg";
 import sklep_pl_i from "./assets/img/sklep_pl.svg";
 import sklep_cz_i from "./assets/img/sklep_cz.svg";
+import czasopismo_pl_i from "./assets/img/czasopismo-pl-i.svg";
+import casopis_cz_i from "./assets/img/casopis-cz-i.svg";
+import list_pl_i from "./assets/img/list-pl-i.svg";
+import list_cz_i from "./assets/img/list-cz-i.svg";
+import zona_pl_i from "./assets/img/zona-pl-i.svg";
+import zena_cz_i from "./assets/img/zona-cz-i.svg";
 
 import cena_pl_s from "./assets/sound/cena_pl.mp3";
 import cena_cz_s from "./assets/sound/cena_cz.mp3";
@@ -39,6 +45,12 @@ import walka_pl_s from "./assets/sound/walka_pl.mp3";
 import valka_cz_s from "./assets/sound/valka_cz.mp3";
 import sklep_pl_s from "./assets/sound/sklep_pl.mp3";
 import sklep_cz_s from "./assets/sound/sklep_cz.mp3";
+import czasopismo_pl_s from "./assets/sound/czasopismo-pl-s.mp3";
+import casopis_cz_s from "./assets/sound/casopis-cz-s.mp3";
+import list_pl_s from "./assets/sound/list-pl-s.mp3";
+import list_cz_s from "./assets/sound/list-cz-s.mp3";
+import zona_pl_s from "./assets/sound/zona-pl-s.mp3";
+import zena_cz_s from "./assets/sound/zena-cz-s.mp3";
 
 const CARD_IMAGES_PL_CZ = [
   {
@@ -181,12 +193,254 @@ const CARD_IMAGES_PL_CZ = [
     sound: valka_cz_s,
     translation: "Czech: war",
   },
+  {
+    id: "011",
+    src: list_pl_i,
+    matched: false,
+    sound: list_pl_s,
+    translation: "Polish: letter",
+  },
+  {
+    id: "011",
+    src: list_cz_i,
+    matched: false,
+    sound: list_cz_s,
+    translation: "Czech: leaf",
+  },
+  {
+    id: "012",
+    src: zona_pl_i,
+    matched: false,
+    sound: zona_pl_s,
+    translation: "Polish: wife",
+  },
+  {
+    id: "012",
+    src: zena_cz_i,
+    matched: false,
+    sound: zena_cz_s,
+    translation: "Czech: woman",
+  },
+  {
+    id: "013",
+    src: czasopismo_pl_i,
+    matched: false,
+    sound: czasopismo_pl_s,
+    translation: "Polish: magazine",
+  },
+  {
+    id: "013",
+    src: casopis_cz_i,
+    matched: false,
+    sound: casopis_cz_s,
+    translation: "Czech: newspaper",
+  },
 ];
 
 export default CARD_IMAGES_PL_CZ;
 
 /*
 
+const CARD_IMAGES_PL_CZ = [
+  {
+    id: "001",
+    src: "./assets/img/zly_pl.svg",
+    matched: false,
+    sound: "./assets/sound/zly_pl.mp3",
+    translation: "Polish: bad, evil",
+  },
+  {
+    id: "001",
+    src: "./assets/img/zly_cz.svg",
+    matched: false,
+    sound: "./assets/sound/zly_cz.mp3",
+    translation: "Czech: bad, but can also mean wicked",
+  },
+  {
+    id: "002",
+    src: "./assets/img/droga_pl.svg",
+    matched: false,
+    sound: "./assets/sound/droga_pl.mp3",
+    translation: "Polish: road, way",
+  },
+  {
+    id: "002",
+    src: "./assets/img/droga_cz.svg",
+    matched: false,
+    sound: "./assets/sound/droga_cz.mp3",
+    translation: "Czech: drug",
+  },
+  {
+    id: "003",
+    src: "./assets/img/cena_pl.svg",
+    matched: false,
+    sound: "./assets/sound/cena_pl.mp3",
+    translation: "Polish: price",
+  },
+  {
+    id: "003",
+    src: "./assets/img/cena_cz.svg",
+    matched: false,
+    sound: "./assets/sound/cena_cz.mp3",
+    translation: "Czech: dinner",
+  },
+  {
+    id: "004",
+    src: "./assets/img/czar.svg",
+    matched: false,
+    sound: "./assets/sound/czar_pl.mp3",
+    translation: "Polish: charm",
+  },
+  {
+    id: "004",
+    src: "./assets/img/car-cz-s.svg",
+    matched: false,
+    sound: "./assets/sound/car_cz.mp3",
+    translation: "Czech: line, stroke",
+  },
+  {
+    id: "005",
+    src: "./assets/img/czerstwy.svg",
+    matched: false,
+    sound: "./assets/sound/czerstwy_pl.mp3",
+    translation: "Polish: stale (as in bread)",
+  },
+  {
+    id: "005",
+    src: "./assets/img/cerstvy.svg",
+    matched: false,
+    sound: "./assets/sound/cerstvy_cz.mp3",
+    translation: "Czech: fresh",
+  },
+  {
+    id: "006",
+    src: "./assets/img/dziadek.svg",
+    matched: false,
+    sound: "./assets/sound/dziadek_pl.mp3",
+    translation: "Polish: grandfather",
+  },
+  {
+    id: "006",
+    src: "./assets/img/dedek.svg",
+    matched: false,
+    sound: "./assets/sound/dedek_cz.mp3",
+    translation: "Czech: old man (can be pejorative)",
+  },
+  {
+    id: "007",
+    src: "./assets/img/lada_pl.svg",
+    matched: false,
+    sound: "./assets/sound/lada_pl.mp3",
+    translation: "Polish: counter, desk",
+  },
+  {
+    id: "007",
+    src: "./assets/img/lada_cz.svg",
+    matched: false,
+    sound: "./assets/sound/lada_cz.mp3",
+    translation: "Czech: meadows",
+  },
+  {
+    id: "008",
+    src: "./assets/img/palec_pl.svg",
+    matched: false,
+    sound: "./assets/sound/palec_pl.mp3",
+    translation: "Polish: finger",
+  },
+  {
+    id: "008",
+    src: "./assets/img/palec_cz.svg",
+    matched: false,
+    sound: "./assets/sound/palec_cz.mp3",
+    translation: "Czech: thumb",
+  },
+  {
+    id: "009",
+    src: "./assets/img/sklep_pl.svg",
+    matched: false,
+    sound: "./assets/sound/sklep_pl.mp3",
+    translation: "Polish: store/shop",
+  },
+  {
+    id: "009",
+    src: "./assets/img/sklep_cz.svg",
+    matched: false,
+    sound: "./assets/sound/sklep_cz.mp3",
+    translation: "Czech: basement",
+  },
+  {
+    id: "010",
+    src: "./assets/img/walka.svg",
+    matched: false,
+    sound: "./assets/sound/walka_pl.mp3",
+    translation: "Polish: fight, struggle",
+  },
+  {
+    id: "010",
+    src: "./assets/img/valka.svg",
+    matched: false,
+    sound: "./assets/sound/valka_cz.mp3",
+    translation: "Czech: war",
+  },
+  {
+    id: "011",
+    src: "./assets/img/list-pl-i.svg",
+    matched: false,
+    sound: "./assets/sound/list-pl-s.mp3",
+    translation: "Polish: letter",
+  },
+  {
+    id: "011",
+    src: "./assets/img/list-cz-i.svg",
+    matched: false,
+    sound: "./assets/sound/list-cz-s.mp3",
+    translation: "Czech: leaf",
+  },
+  {
+    id: "012",
+    src: "./assets/img/zona-pl-i.svg",
+    matched: false,
+    sound: "./assets/sound/zona-pl-s.mp3",
+    translation: "Polish: wife",
+  },
+  {
+    id: "012",
+    src: "./assets/img/zona-cz-i.svg",
+    matched: false,
+    sound: "./assets/sound/zena-cz-s.mp3",
+    translation: "Czech: woman",
+  },
+  {
+    id: "013",
+    src: "./assets/img/czasopismo-pl-i.svg",
+    matched: false,
+    sound: "./assets/sound/czasopismo-pl-s.mp3",
+    translation: "Polish: magazine",
+  },
+  {
+    id: "013",
+    src: "./assets/img/casopis-cz-i.svg",
+    matched: false,
+    sound: "./assets/sound/casopis-cz-s.mp3",
+    translation: "Czech: newspaper",
+  },
+];
+
+
+----------------------------------------------------------------------------------------
+
+import cena_pl_i from "./assets/img/cena_pl.svg";
+import cena_cz_i from "./assets/img/cena_cz.svg";
+import czar_pl_i from "./assets/img/czar.svg";
+import car_cz_i from "./assets/img/car-cz-s.svg";
+import droga_pl_i from "./assets/img/droga_pl.svg";
+import droga_cz_i from "./assets/img/droga_cz.svg";
+import zly_pl_i from "./assets/img/zly_pl.svg";
+import zly_cz_i from "./assets/img/zly_cz.svg";
+import czerstwy_pl_i from "./assets/img/czerstwy.svg";
+import cerstvy_cz_i from "./assets/img/cerstvy.svg";
+import dziadek_pl_i from "./assets/img/dziadek.svg";
+import dedek_cz_i from "./assets/img/dedek.svg";
 import lada_pl_i from "./assets/img/lada_pl.svg";
 import lada_cz_i from "./assets/img/lada_cz.svg";
 import palec_pl_i from "./assets/img/palec_pl.svg";
@@ -195,7 +449,25 @@ import walka_pl_i from "./assets/img/walka.svg";
 import valka_cz_i from "./assets/img/valka.svg";
 import sklep_pl_i from "./assets/img/sklep_pl.svg";
 import sklep_cz_i from "./assets/img/sklep_cz.svg";
+import czasopismo_pl_i from "./assets/img/czasopismo-pl-i.svg";
+import casopis_cz_i from "./assets/img/casopis-cz-i.svg";
+import list_pl_i from "./assets/img/list-pl-i.svg";
+import list_cz_i from "./assets/img/list-cz-i.svg";
+import zona_pl_i from "./assets/img/zona-pl-i.svg";
+import zena_cz_i from "./assets/img/zona-cz-i.svg";
 
+import cena_pl_s from "./assets/sound/cena_pl.mp3";
+import cena_cz_s from "./assets/sound/cena_cz.mp3";
+import czar_pl_s from "./assets/sound/czar_pl.mp3";
+import car_cz_s from "./assets/sound/car_cz.mp3";
+import droga_pl_s from "./assets/sound/droga_pl.mp3";
+import droga_cz_s from "./assets/sound/droga_cz.mp3";
+import zly_pl_s from "./assets/sound/zly_pl.mp3";
+import zly_cz_s from "./assets/sound/zly_cz.mp3";
+import czerstwy_pl_s from "./assets/sound/czerstwy_pl.mp3";
+import cerstvy_cz_s from "./assets/sound/cerstvy_cz.mp3";
+import dziadek_pl_s from "./assets/sound/dziadek_pl.mp3";
+import dedek_cz_s from "./assets/sound/dedek_cz.mp3";
 import lada_pl_s from "./assets/sound/lada_pl.mp3";
 import lada_cz_s from "./assets/sound/lada_cz.mp3";
 import palec_pl_s from "./assets/sound/palec_pl.mp3";
@@ -204,16 +476,199 @@ import walka_pl_s from "./assets/sound/walka_pl.mp3";
 import valka_cz_s from "./assets/sound/valka_cz.mp3";
 import sklep_pl_s from "./assets/sound/sklep_pl.mp3";
 import sklep_cz_s from "./assets/sound/sklep_cz.mp3";
+import czasopismo_pl_s from "./assets/sound/czasopismo-pl-s.mp3";
+import casopis_cz_s from "./assets/sound/casopis-cz-s.mp3";
+import list_pl_s from "./assets/sound/list-pl-s.mp3";
+import list_cz_s from "./assets/sound/list-cz-s.mp3";
+import zona_pl_s from "./assets/sound/zona-pl-s.mp3";
+import zena_cz_s from "./assets/sound/zena-cz-s.mp3";
 
-{ id: "007", src: lada_pl_i, matched: false, sound: lada_pl_s, translation: "Polish: counter, desk" },
-{ id: "007", src: lada_cz_i, matched: false, sound: lada_cz_s, translation: "Czech: meadows" },
-{ id: "008", src: palec_pl_i, matched: false, sound: palec_pl_s, translation: "Polish: finger" },
-{ id: "008", src: palec_cz_i, matched: false, sound: palec_cz_s, translation: "Czech: thumb" },
-{ id: "009", src: sklep_pl_i, matched: false, sound: sklep_pl_s, translation: "Polish: store/shop" },
-{ id: "009", src: sklep_cz_i, matched: false, sound: sklep_cz_s, translation: "Czech: basement" },
-{ id: "010", src: walka_pl_i, matched: false, sound: walka_pl_s, translation: "Polish: fight, struggle" },
-{ id: "010", src: valka_cz_i, matched: false, sound: valka_cz_s, translation: "Czech: war" },
+const CARD_IMAGES_PL_CZ = [
+  {
+    id: "001",
+    src: zly_pl_i,
+    matched: false,
+    sound: zly_pl_s,
+    translation: "Polish: bad, evil",
+  },
+  {
+    id: "001",
+    src: zly_cz_i,
+    matched: false,
+    sound: zly_cz_s,
+    translation: "Czech: bad, but can also mean wicked",
+  },
+  {
+    id: "002",
+    src: droga_pl_i,
+    matched: false,
+    sound: droga_pl_s,
+    translation: "Polish: road, way",
+  },
+  {
+    id: "002",
+    src: droga_cz_i,
+    matched: false,
+    sound: droga_cz_s,
+    translation: "Czech: drug",
+  },
+  {
+    id: "003",
+    src: cena_pl_i,
+    matched: false,
+    sound: cena_pl_s,
+    translation: "Polish: price",
+  },
+  {
+    id: "003",
+    src: cena_cz_i,
+    matched: false,
+    sound: cena_cz_s,
+    translation: "Czech: dinner",
+  },
+  {
+    id: "004",
+    src: czar_pl_i,
+    matched: false,
+    sound: czar_pl_s,
+    translation: "Polish: charm",
+  },
+  {
+    id: "004",
+    src: car_cz_i,
+    matched: false,
+    sound: car_cz_s,
+    translation: "Czech: line, stroke",
+  },
+  {
+    id: "005",
+    src: czerstwy_pl_i,
+    matched: false,
+    sound: czerstwy_pl_s,
+    translation: "Polish: stale (as in bread)",
+  },
+  {
+    id: "005",
+    src: cerstvy_cz_i,
+    matched: false,
+    sound: cerstvy_cz_s,
+    translation: "Czech: fresh",
+  },
+  {
+    id: "006",
+    src: dziadek_pl_i,
+    matched: false,
+    sound: dziadek_pl_s,
+    translation: "Polish: grandfather",
+  },
+  {
+    id: "006",
+    src: dedek_cz_i,
+    matched: false,
+    sound: dedek_cz_s,
+    translation: "Czech: old man (can be pejorative)",
+  },
+  {
+    id: "007",
+    src: lada_pl_i,
+    matched: false,
+    sound: lada_pl_s,
+    translation: "Polish: counter, desk",
+  },
+  {
+    id: "007",
+    src: lada_cz_i,
+    matched: false,
+    sound: lada_cz_s,
+    translation: "Czech: meadows",
+  },
+  {
+    id: "008",
+    src: palec_pl_i,
+    matched: false,
+    sound: palec_pl_s,
+    translation: "Polish: finger",
+  },
+  {
+    id: "008",
+    src: palec_cz_i,
+    matched: false,
+    sound: palec_cz_s,
+    translation: "Czech: thumb",
+  },
+  {
+    id: "009",
+    src: sklep_pl_i,
+    matched: false,
+    sound: sklep_pl_s,
+    translation: "Polish: store/shop",
+  },
+  {
+    id: "009",
+    src: sklep_cz_i,
+    matched: false,
+    sound: sklep_cz_s,
+    translation: "Czech: basement",
+  },
+  {
+    id: "010",
+    src: walka_pl_i,
+    matched: false,
+    sound: walka_pl_s,
+    translation: "Polish: fight, struggle",
+  },
+  {
+    id: "010",
+    src: valka_cz_i,
+    matched: false,
+    sound: valka_cz_s,
+    translation: "Czech: war",
+  },
+  {
+    id: "011",
+    src: list_pl_i,
+    matched: false,
+    sound: list_pl_s,
+    translation: "Polish: letter",
+  },
+  {
+    id: "011",
+    src: list_cz_i,
+    matched: false,
+    sound: list_cz_s,
+    translation: "Czech: leaf",
+  },
+  {
+    id: "012",
+    src: zona_pl_i,
+    matched: false,
+    sound: zona_pl_s,
+    translation: "Polish: wife",
+  },
+  {
+    id: "012",
+    src: zena_cz_i,
+    matched: false,
+    sound: zena_cz_s,
+    translation: "Czech: woman",
+  },
+  {
+    id: "013",
+    src: czasopismo_pl_i,
+    matched: false,
+    sound: czasopismo_pl_s,
+    translation: "Polish: magazine",
+  },
+  {
+    id: "013",
+    src: casopis_cz_i,
+    matched: false,
+    sound: casopis_cz_s,
+    translation: "Czech: newspaper",
+  },
+];
 
+export default CARD_IMAGES_PL_CZ;
 
 
 */
